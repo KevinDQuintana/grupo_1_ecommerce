38805bc97f7236a0adacd9281c8655710b149570
KevinDQuintana <kevinquintanaunjufi@gmail.com>
2023-07-29 23:42:01 -0300

    Agrego la estructura inicial del "Home" y normalizo el estilo del "app.js".

--- project/src/app.js
+++ project/src/app.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
-const publicPath = path.resolve(__dirname, './public');
+const PORT = process.env.PORT || 4000;
+const publicPath = path.resolve(__dirname, '../', 'public');
 
 app.use(express.static(publicPath));
-app.listen (4000, () => console.log ("servidor andando"));
-app.get ('/', function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-   });
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+});
