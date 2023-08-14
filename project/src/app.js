726772ab0482ff2019c68c320c07a485a825813b
valelopee8 <valelopee8@gmail.com>
2023-08-14 16:29:47 -0300

    Agrego la ruta productsOptions y completo las rutas de require con path.join()

--- project/src/app.js
+++ project/src/app.js
@@ -7,12 +7,14 @@ const publicPath = path.resolve(__dirname, '../', 'public');
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs');
 
-const ruteIndex = require(path.join(__dirname, '../', 'routes', 'index'))
-const ruteProductCart = require('../routes/productCart');
-const ruteProductDetail = require('../routes/productDetail');
+const ruteIndex = require(path.join(__dirname, '../', 'routes', 'index'));
+const ruteProductCart = require(path.join(__dirname, '../', 'routes', 'productCart'));
+const ruteProductDetail = require(path.join(__dirname, '../', 'routes', 'productDetail'));
+const ruteProductOptions = require(path.join(__dirname, '../', 'routes', 'productOptions'));
 
 app.use('/', ruteIndex);
 app.use('/productCart', ruteProductCart);
 app.use('/productDetail', ruteProductDetail);
+app.use('/productOptions',ruteProductOptions);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
