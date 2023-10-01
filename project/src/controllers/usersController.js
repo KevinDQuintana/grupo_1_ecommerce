5b25be761170270fb907f5828a73f395b5ab45a4
Valentín López <valelopee8@gmail.com>
2023-10-01 00:06:02 -0300

    Función 'processRegister' para registrar nuevos usuarios y añadirlos en users.json

--- project/src/controllers/usersController.js
+++ project/src/controllers/usersController.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
 const path = require('path');
+const usersFilePath = path.join(__dirname, '../', 'data', 'users.json');
+const { validationResult } = require('express-validator');
+
+function getUsers() {
+	return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+}
 
 const controller = {
 
@@ -7,7 +14,29 @@ const controller = {
 	},
 
 	signup: function (req, res) {
-		res.render(path.join(__dirname, '../', 'views', 'users', 'signup'), { styles: ['/css/index.css', '/css/signup.css'] });
+		return res.render(path.join(__dirname, '../', 'views', 'users', 'signup'), { styles: ['/css/index.css', '/css/signup.css'] });
+	},
+	processRegister: function (req,res) {
+		const resultValidation = validationResult(req);
+		if (resultValidation.errors.length > 0) {
+			res.render(path.join(__dirname, '../', 'views', 'users', 'signup'), { styles: ['/css/index.css', '/css/signup.css'] , errors: resultValidation.mapped() , oldData: req.body});
+		}
+		const users = getUsers();
+		const newId = users[users.length - 1].id + 1;
+		const newUser = {
+			id: newId,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			password: req.body.password,
+			dni: req.body.dni,
+			phone: req.body.phone,
+			category: req.body.category,
+			image: req.file.filename
+		};
+		users.push(newUser);
+		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, '\t'));
+		return res.redirect('/users');
 	}
 }
 
