42bc237845ae17308447aee8b6b723a8e62d4b47
SantinoViglino <SantinoViglino>
2023-11-08 20:45:15 -0300

    agrego sequelize a processSignup

--- project/src/controllers/usersController.js
+++ project/src/controllers/usersController.js
@@ -28,52 +28,7 @@ const controller = {
 		return res.render(path.join(__dirname, '..', 'views', 'users', 'profile'), { styles: ['/css/index.css'], user: req.session.userLogged })
 	},
 
-	processLogin: function (req, res) {
-		console.log('POST Request');
-		console.log(req.body);
-
-		const errors = validationResult(req);
-
-		if (errors.isEmpty()) {
-			const users = getUsers();
-			const requiredUser = users.find(user => user.email === req.body.email);
-
-			if (requiredUser) {
-				console.log('User Found')
-				console.log(`Required User Password: ${requiredUser.password}`);
-
-				if (bcryptjs.compareSync(req.body.password, requiredUser.password)) {
-					console.log('User Password is Correct')
-					req.session.userLogged = requiredUser;
-				} else {
-					console.log('User Password is Incorrect')
-					return res.render(path.join(__dirname, '..', 'views', 'users', 'login'), { styles: ['/css/index.css', '/css/login.css'], validation: { email: { msg: 'Credenciales inválidas' } }, oldData: req.body });
-				}
-			} else {
-				console.log('User Not Found')
-				return res.render(path.join(__dirname, '..', 'views', 'users', 'login'), { styles: ['/css/index.css', '/css/login.css'], validation: { email: { msg: 'No se encuentra este email' } }, oldData: req.body });
-			}
-			console.log('Successful Login')
-			req.session.user = {
-				id: requiredUser.id,
-				email: requiredUser.email,
-				fName: requiredUser.firstName,
-				lName: requiredUser.lastName,
-			};
-
-			if (req.body.rememberMe) {
-				console.log('The user wants to be remembered.');
-				res.cookie('session', req.session.user, { maxAge: 900000 });
-				console.log('Cookie Set');
-			}
-			return res.redirect('/')
-		} else {
-			console.log('Login Errors:');
-			console.log(errors);
-			return res.render(path.join(__dirname, '../', 'views', 'users', 'login'), { styles: ['/css/index.css', '/css/login.css'], errors: errors.mapped(), oldData: req.body })
-		}
-
-	},
+	
 	processLogin: function (req, res) {
 		console.log('POST Request');
 		console.log(req.body);
@@ -116,24 +71,32 @@ const controller = {
 	processSignup: function (req, res) {
 		const resultValidation = validationResult(req);
 		if (resultValidation.errors.length > 0) {
-			res.render(path.join(__dirname, '../', 'views', 'users', 'signup'), { styles: ['/css/index.css', '/css/signup.css'], errors: resultValidation.mapped(), oldData: req.body });
+			return res.render(path.join(__dirname, '../', 'views', 'users', 'signup'), { styles: ['/css/index.css', '/css/signup.css'], errors: resultValidation.mapped(), oldData: req.body });
 		}
-		const users = getUsers();
-		const newId = users[users.length - 1].id + 1;
-		const newUser = {
-			id: newId,
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
+		let categoryId = '';
+		if(req.body.category == 'Admin'){
+			categoryId = 1
+		}
+		if(req.body.category == 'Vendedor'){
+			categoryId = 2
+		}
+		if(req.body.category == 'Comprador'){
+			categoryId = 3
+		}
+		if(req.body.category == 'Empresa'){
+			categoryId = 4
+		}
+		db.Users.create({
+			first_name: req.body.firstName,
+			last_name: req.body.lastName,
 			email: req.body.email,
 			password: bcryptjs.hashSync(req.body.password, 10),
 			dni: req.body.dni,
 			phone: req.body.phone,
-			category: req.body.category,
+			category_id: categoryId,
 			image: req.file.filename
-		};
-		users.push(newUser);
-		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, '\t'));
-		return res.redirect('/');
+		})
+		.then(()=>{return res.redirect('/')})
 	}
 }
 
